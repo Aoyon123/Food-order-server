3345a29036cfa5e6089b7ebc290e43eb82b84826
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2022-01-01 02:55:18 +0600

    databasedatacollection

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 const express = require("express");
-const mysql = require("mysql");
+// const mysql = require("mysql");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://Fahamidul:aoyonabc@cluster0.dxczq.mongodb.net/food?retryWrites=true&w=majority";
+
+app.listen("5000", () => {
+  console.log("server started on port 5000");
+});
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  console.log("error", err);
+  const collection = client.db("food").collection("tblUsers");
+
+  console.log("database Connected");
+  app.get("/users", (req, res) => {
+    collection.find().toArray((err, result) => {
+      res.send(result);
+    });
+  });
+  app.post("/addUser", (req, res) => {
+    const newUser = req.body;
+    console.log("adding new user ", newUser);
+    collection.insertOne(newUser).then((result) => {
+      console.log("inserted count", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    collection
+      .findOne({ username: username, password: password })
+      .then((result) => {
+        console.log("result count", result);
+        res.send(result);
+      });
+  });
+  // perform actions on the collection object
+});
+
+/*
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "foodDb",
 });
+
 db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log("db connected");
 });
-app.listen("5000", () => {
-  console.log("server started on port 5000");
-});
-app.get("/addUser", (req, res) => {
+
+app.post("/addUser", (req, res) => {
   let post = {
     dateOfBirth: "2021-12-30",
     email: "fahamidulaoyon@gmail.com",
     fullName: "ssfgk",
     password: "1234569",
-    username: "nihal",
+    username: "nihal1",
   };
   let sql = "INSERT INTO tblUsers SET ?";
-  let query = db.query(sql, post, (err, result) => {
+  let query = db.query(sql, req.body, (err, result) => {
     if (err) {
       throw err;
     }
@@ -33,13 +79,39 @@ app.get("/addUser", (req, res) => {
     res.send("New user created");
   });
 });
+// app.post("/addUser", (req, res) => {
+//   const newUser = req.body;
+//   var sql =
+//     "SET @fullName = ?;SET @email = ?;SET @dateOfBirth = ?;SET @username = ?; SET @password = ?; \
+//     CALL tblUsers(@fullName,@email,@dateOfBirth,@username, @password);";
+//   let query = db.query(
+//     sql,
+//     [
+//       newUser.fullName,
+//       newUser.email,
+//       newUser.dateOfBirth,
+//       newUser.username,
+//       newUser.password,
+//     ],
+//     (err, rows, fields) => {
+//       if (err) {
+//         console.log(err);
+//         throw err;
+//       } else {
+//         console.log("New User created", rows);
+//         res.send("New user created");
+//       }
+//     }
+//   );
+// });
+
 app.get("/users", (req, res) => {
   let sql = "SELECT * FROM tblUsers";
   let query = db.query(sql, (err, result) => {
     if (err) {
-      throw err;
+      console.log(err)
     }
     console.log(result);
     res.send(result);
   });
-});
+});*/
